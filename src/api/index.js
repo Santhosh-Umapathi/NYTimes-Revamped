ffffc74893e6eaeaf55c1aae3960f96d7cdd963f
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2022-01-27 17:45:55 +0100

    FIX: API updated

--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,17 @@ import axios from "axios";
 const API_KEY = "&api-key=" + process.env.REACT_APP_NYTIMES_API_KEY;
 const BASE_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-export const api = async ({ url = "", query = "", fields = "" }) => {
-  const q = "?q=" + query;
-  const fl = "&fl=" + fields;
-  const response = await axios.get(BASE_URL + q + fl + API_KEY);
+export const api = async ({
+  query = "",
+  fields = "",
+  page = 0,
+  filterQuery = "",
+}) => {
+  const q = query && "?q=" + query;
+  const fl = fields && "&fl=" + fields;
+  const pg = "&page=" + page;
+  const fq = filterQuery && "?fq=" + filterQuery;
+  const response = await axios.get(BASE_URL + q + fq + fl + pg + API_KEY);
 
   return response.data;
 };
