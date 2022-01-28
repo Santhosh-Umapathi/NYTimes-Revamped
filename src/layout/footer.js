f909a54563b38e1af8246d1deda0648aa7064a2a
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2022-01-28 16:40:56 +0100

    FIX: Footer & Splash updated

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,16 +1,23 @@
 import { memo } from "react";
+import { useLocation } from "react-router-dom";
 //Icons
 import { Copyright, Github, LinkedinIcon } from "../components/icons";
 
-const Footer = () => (
-  <div
-    className={`flex items-center h-12 fixed bottom-0 bg-primary w-full space-x-2 px-10 text-bgLight`}
-  >
-    <Copyright />
-    <span>Santhosh Umapathi</span>
-    <Github />
-    <LinkedinIcon />
-  </div>
-);
+const Footer = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div
+      className={`${
+        pathname === "/" ? "hidden" : "flex"
+      } items-center h-12 fixed bottom-0 bg-primary w-full space-x-2 px-10 text-bgLight`}
+    >
+      <Copyright />
+      <span>Santhosh Umapathi</span>
+      <Github />
+      <LinkedinIcon />
+    </div>
+  );
+};
 
 export default memo(Footer);
--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -2,23 +2,40 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 //Logo
 import { Logo } from "../components";
+//Recoil
+import { useAtoms } from "../recoil/hooks";
 
 const Splash = () => {
   const navigate = useNavigate();
 
+  const {
+    state: { theme },
+  } = useAtoms();
+
   // Show Splash Screen- Fist time only
   useEffect(() => {
     const isSplashLoaded = localStorage.getItem("splash");
-    isSplashLoaded === "yes" && navigate("/home");
+    isSplashLoaded === "yes" && navigate("/home?page=0");
     localStorage.setItem("splash", "yes");
     setTimeout(() => {
-      navigate("/home");
-    }, 3000);
+      navigate("/home?page=0");
+    }, 2000);
   }, []);
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-bgLight">
-      <Logo css="w-[500px] animate-pulse" cssText="animate-pulse" />
+    <div
+      className={`flex justify-center items-center w-screen h-screen ${
+        theme === "dark" ? "bg-bgDark" : "bg-bgLight"
+      } `}
+    >
+      <Logo
+        css={`w-[500px] animate-pulse ${
+          theme === "dark" ? "fill-bgLight" : "fill-grey"
+        }  `}
+        cssText={`animate-pulse ${
+          theme === "dark" ? "text-bgLight" : "text-grey"
+        }`}
+      />
     </div>
   );
 };
